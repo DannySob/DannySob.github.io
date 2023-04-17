ea105b50b7d2eb610714905e4c028a6e0c1e0ccf
Daniel Sobczak <115968699+DannySob@users.noreply.github.com>
2023-04-16 23:59:26 -0600

    Update wa12.js

--- js/wa12.js
+++ js/wa12.js
@@ -3,16 +3,16 @@ let emp = '{"employees": [' +
 ' {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": "18,500", "raise eligable": "True" },' +
 ' {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": "21,200", "raise eligable": "False"} ]}';
 
-const employees = JSON.parse(emp);
+const employ = JSON.parse(emp);
 
 console.log("Problem 1");
-console.log(employees);
+console.log(employ);
 
 let com = '{"company": [' +
 ' {"companyname": "Tech Stars", "website": "www.techstars.site", "employees": [' + 
-' {"firstname": "Sam", "department": "Tech", "designation": "Manager", "salary": "40,000", "raise eligable": "True" },' +
-' {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": "18,500", "raise eligable": "True" },' +
-' {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": "21,200", "raise eligable": "False"} ]}]}';
+' {"firstname": "Sam", "department": "Tech", "designation": "Manager", "salary": 40000, "raise_eligable": "True" },' +
+' {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": 18500, "raise_eligable": "True" },' +
+' {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": 21200, "raise_eligable": "False"} ]}]}';
 
 const company = JSON.parse(com);
 
@@ -20,12 +20,32 @@ console.log("Problem 2");
 console.log(company);
 
 let emp2 = '{"employees": [' + 
-' {"firstname": "Sam", "department": "Tech", "designation": "Manager", "salary": "40,000", "raise eligable": "True" },' +
-' {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": "18,500", "raise eligable": "True" },' +
-' {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": "21,200", "raise eligable": "False"},' +
-' {"firstname": "Anna", "department": "Tech", "designation": "Executive", "salary": "25,600", "raise eligable": "False"} ]}';
+' {"firstname": "Sam", "department": "Tech", "designation": "Manager", "salary": 40000, "raise_eligable": "True" },' +
+' {"firstname": "Mary", "department": "Finance", "designation": "Trainee", "salary": 18500, "raise_eligable": "True" },' +
+' {"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": 21200, "raise_eligable": "False"},' +
+' {"firstname": "Anna", "department": "Tech", "designation": "Executive", "salary": 25600, "raise_eligable": "False"} ]}';
 
-const employees2 = JSON.parse(emp2);
+const employ2 = JSON.parse(emp2);
 
 console.log("Problem 3");
-console.log(employees2);
+console.log(employ2);
+
+let sal = employ2["employees"][0].salary + employ2["employees"][1].salary + employ2["employees"][2].salary + employ2["employees"][3].salary;
+
+console.log("Problem 4");
+console.log(sal); 
+
+function salaryUpdate() {
+    for (let i = 0; i < employ2["employees"][i].salary.length; i++) {
+        if (employ2["employees"][i].raise_eligable === 'True') {
+            employ2["employees"][i].salary *= 1.1;
+            employ2["employees"][i].raise_eligable = 'False';
+            return;
+        }
+    }
+}
+
+salaryUpdate();
+
+console.log("Problem 5");
+console.log(employ2);
